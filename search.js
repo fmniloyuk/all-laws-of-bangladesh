4d2285c5c9b6af789faf888d07e5108b7107ab35
sadmanalam142 <sadmanalam142@gmail.com>
2023-02-23 22:09:33 +0600

    search items added

--- search.js
+++ search.js
@@ -1,8 +1,127 @@
 function openPage() {
     var a = document.getElementById("search").value;
 
+    // general laws
     if (a === "বাংলাদেশের সংবিধান") {
         window.open("general-laws/bangladesh-songbidhan.html");
+    }
+
+    if (a === "দেওয়ানী কার্যবিধি আইন, ১৯০৮") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "ফৌজদারি কার্যবিধি আইন, ১৮৯৮") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "দণ্ডবিধি আইন ,১৮৬০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "সাক্ষ্য আইন, ১৯৭২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "তামাদি আইন, ১৯০৮") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "সুনির্দিষ্ট প্রতিকার আইন ১৮৭৭") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "নারী ও শিশু নির্যাতন দমন আইন, ২০০০") {
+        window.open("general-laws/nari-o-sisu-nirzaton-domon-ayin-2000.html");
+    }
+
+    if (a === "বিশেষ ক্ষমতা আইন, ১৯৭৪") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "নেগোশিয়েবল ইনস্ট্রুমেন্ট অ্যাক্ট, ১৮৮১") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "অস্ত্র আইন, ১৮৭৮") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মাদকদ্রব্য নিয়ন্ত্রন আইন, ২০১৮") {
+        window.open("general-laws/madokdrobbo-niyontron-ayin-2018.html");
+    }
+
+    if (a === "তথ্য ও যোগাযোগ প্রযুক্তি আইন, ২০০৬") {
+        window.open("general-laws/totho-o-jogajog-projukti-ayin-2006.html");
+    }
+
+    if (a === "ডিজিটাল নিরাপত্তা আইন, ২০১৮") {
+        window.open("general-laws/digital-nirapotta-ayin-2018.html");
+    }
+
+    if (a === "যৌতুক নিরোধ আইন, ২০১৮") {
+        window.open("general-laws/joutuk-nirodh-ayin-2018.html");
+    }
+
+    if (a === "দুর্নীতি দমন কমিশন আইন, ২০০৪") {
+        window.open("general-laws/durniti-domon-komishon-ayin-2004.html");
+    }
+
+    if (a === "মানব পাচার প্রতিরোধ ও দমন আইন, ২০১২") {
+        window.open("general-laws/manob-pachar-protikar-o-domon-ayin-2012.html");
+    }
+
+    if (a === "মানিলন্ডারিং প্রতিরোধ আইন, ২০১২") {
+        window.open("general-laws/money-laundering-protirodh-ayin-2012.html");
+    }
+
+    if (a === "অর্থ ঋণ আদালত আইন, ২০০৩") {
+        window.open("general-laws/artho-rin-adalot-ayin-2003.html");
+    }
+
+    if (a === "শিশু আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "জাতীয় পরিচয় নিবন্ধন আইন, ২০১০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "পুলিশ আইন, ১৮৬১") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "বিস্ফোরক দ্রব্য আইন, ১৯০৮") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "পারিবারিক আদালত অধ্যাদেশ, ১৯৮৫") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মুসলিম পারিবারিক আইন অধ্যাদেশ, ১৯৬১") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মুসলিম বিবাহ বিচ্ছেদ আইন, ১৯৩৯") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মুসলিম বিবাহ ও তালাক( রেজিস্ট্রিকরণ) আইন, ১৯৭৪") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "বাল্যবিবাহ বিরোধ আইন, ১৯২৯") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "প্রকাশ্য জুয়া আইন, ১৮৬৭") {
+        window.open("404-not-found.html");
+    }
+
+    // latest laws
+
+    else {
+        window.open("404-not-found.html");
     }
 
 }

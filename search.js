fa872be9b528c87d9bb10ab1d5d6f4086c0f1ea0
sadmanalam142 <sadmanalam142@gmail.com>
2023-02-23 18:28:20 +0600

    search bar added

---
+++ search.js
@@ -0,0 +1,8 @@
+function openPage() {
+    var a = document.getElementById("search").value;
+
+    if (a === "বাংলাদেশের সংবিধান") {
+        window.open("general-laws/bangladesh-songbidhan.html");
+    }
+
+}

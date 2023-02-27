c88734a93341c660e075ce010eba37e5c9db5e74
sadmanalam142 <sadmanalam142@gmail.com>
2023-02-27 23:30:27 +0600

    general and latest law added and search items also added

--- search.js
+++ search.js
@@ -118,7 +118,159 @@ function openPage() {
         window.open("404-not-found.html");
     }
 
+    if (a === "দ্রুত বিচার ট্রাইব্যুনাল আইন, ২০০২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "পর্নোগ্রাফি নিয়ন্ত্রণ আইন, ২০১২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মানব পাচার প্রতিরোধ ও দমন আইন, ২০১২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "এসিড নিয়ন্ত্রণ (সংশোধন) আইন, ২০১০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মানিলন্ডারিং প্রতিরোধ আইন, ২০১২ [সংশোধন-২০১৫)") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "নির্যাতন এবং হেফাজতে মৃত্যু (নিবারণ) আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "পাসপোর্ট আইন, ১৯২০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "বাংলাদেশ পাসপোর্ট আদেশ, ১৯৭৩") {
+        window.open("404-not-found.html");
+    }
+
     // latest laws
+    if (a === "আইন-শৃঙ্খলা বিঘ্নকারী অপরাধ (দ্রুত বিচার) (সংশোধন) আইন, ২০১৮") {
+        window.open("404-not-found.html");
+    }
+    
+    if (a === "পারিবারিক সহিংসতা (প্রতিরোধ ও সুরক্ষা) আইন, ২০১০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "দুর্নীতি দমন কমিশন আইন, ২০০৪ (সংশোধন- ২০১৬)") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "অপরাধ সম্পর্কিত বিষয়ে পারস্পরিক সহায়তা আইন, ২০১২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "আদালত অবমাননা আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "নারী ও শিশু নির্যাতন দমন (সংশোধন- ২০২০)") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "বিস্ফোরক উপাদানবলী (সংশোধন) আইন, ২০০২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মোবাইল কোর্ট আইন. ২০০৯ ( সংশোধন) আইন ২০১৭") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "তথ্য ও যোগাযোগ প্রযুক্তি আইন, ২০০৬ (সংশোধন) আইন-২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "ভোক্ত-অধিকার সংরক্ষণ আইন, ২০০৯") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "দেওয়াল লিখন ও পোস্টার লাগানো (নিয়ন্ত্রন) আইন, ২০১২") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "ইট প্রস্তুত ও ভাটা স্থাপন ( নিয়ন্ত্রণ) আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "পিতা-মাতার ভরণ-পোষণ আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "ধুমপান ও তামাকজাত দ্রব্যের ব্যবহার ( নিয়ন্ত্রণ) আইন- ২০০৫( সংশোধন) ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "নিরাপদ খাদ্য আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "পরিবেশ আদালত আইন, ২০১০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "বাংলাদেশ পরিবেশ সংরক্ষণ আইন, ১৯৯৫ ( সংশোধন) আইন ২০১০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মাতৃদুগ্ধ বিকল্প, শিশু খাদ্য, বাণিজ্যিকভাবে প্রস্তুতকৃত শিশুর বাড়তি খাদ্য ও উহা ব্যবহারের সারঞ্জামাদি (বিপণন নিয়ন্ত্রণ) আইন, ২০১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "রেলওয়ে আইন, ১৮৯০") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "জাতীয় মানবাধিকার কমিশন আইন, ২০০৯") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মুসলিম বিবাহ ও তালাক (নিবদ্ধন) বিধিমালা, ২০০৯ (সংশোধনী-২০১১)") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "হস্তান্তরযোগ্য দলিল আইন, ১৮৮১") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "হস্তান্তরযোগ্য দলিল (সংশোধন আইন) ২০০৬") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "সন্ত্রাস বিরোধী আইন, ২০০৯ {সন্ত্রাস বিরোধী (সংশোধন) আইন, ২০১৩}") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "অ-কৃষি প্রজাস্বত্ব আইন, ১৯৪৯") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "বাড়ী ভাড়া নিয়ন্ত্রণ আইন, ১৯৯১") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "কোর্ট ফিস আইন, ১৮৭০(সংশোধন-২০১৬)") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "মামলা মূল্যায়ণ আইন- ১৮৮৭বাংলাদেশ পাসপোর্ট আদেশ, ৯৭১৩") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "ক্ষুদ্র বিচার বিষয়ক আদালত আইন, ১৮৮৭") {
+        window.open("404-not-found.html");
+    }
+
+    if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
+        window.open("404-not-found.html");
+    }
+    
 
     else {
         window.open("404-not-found.html");

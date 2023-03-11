beedabcb02b56838121857ee9880a08575154de0
sadmanalam142 <sadmanalam142@gmail.com>
2023-03-11 20:31:29 +0600

    all the hindu widows remarriage act 1856 laws have been added

--- search.js
+++ search.js
@@ -484,6 +484,10 @@ function openPage() {
         window.open("english-laws/the-arms-act-1878.html");
     }
 
+    if (a === "The Hindu Widow's Re-marriage Act, 1856") {
+        window.open("english-laws/the-hindu-widows-remarriage-act-1856.html");
+    }
+
     if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
         window.open("404-not-found.html");
     }

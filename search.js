3145ddbc59f89eb79029addf9d95f92d382e0121
sadmanalam142 <sadmanalam142@gmail.com>
2023-03-15 15:23:44 +0600

    all the passport offences act 1952 laws have been added

--- search.js
+++ search.js
@@ -604,6 +604,10 @@ function openPage() {
         window.open("english-laws/the-army-and-air-force-reserves-act-1950.html");
     }
 
+    if (a === "The Passport (Offences) Act, 1952") {
+        window.open("english-laws/the-passport-offences-act-1952.html");
+    }
+
     if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
         window.open("404-not-found.html");
     }

ca961570184c4eeedaffc64991e98d16774cd095
sadmanalam142 <sadmanalam142@gmail.com>
2023-03-16 14:57:53 +0600

    all the bangladesh insurance nationalisation order 1972 laws have been added

--- search.js
+++ search.js
@@ -632,6 +632,10 @@ function openPage() {
         window.open("english-laws/the-bangladesh-currency-order-1972.html");
     }
 
+    if (a === "The Bangladesh Insurance (Nationalisation) Order, 1972 (President's Order)") {
+        window.open("english-laws/the-bangladesh-insurance-nationalisation-order-1972.html");
+    }
+
     if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
         window.open("404-not-found.html");
     }

bb29f710bb65bc996dbf1b21ee41f7fad7b4a938
sadmanalam142 <sadmanalam142@gmail.com>
2023-03-19 01:06:28 +0600

    all the muslim marriages and divorces registration act 1974 laws have been added

--- search.js
+++ search.js
@@ -672,6 +672,10 @@ function openPage() {
         window.open("english-laws/the-special-powers-act-1974.html");
     }
 
+    if (a === "The Muslim Marriages and Divorces (Registration) Act, 1974") {
+        window.open("english-laws/the-muslim-marriages-and-divorces-registration-act-1974.html");
+    }
+
     if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
         window.open("404-not-found.html");
     }

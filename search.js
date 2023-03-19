95a739e0be885464c459a82ea8348eb2ede79257
sadmanalam142 <sadmanalam142@gmail.com>
2023-03-19 17:10:52 +0600

    all the land development tax ordinance 1976 laws have been added

--- search.js
+++ search.js
@@ -684,6 +684,10 @@ function openPage() {
         window.open("english-laws/the-pharmacy-ordinance-1976.html");
     }
 
+    if (a === "The Land Development Tax Ordinance, 1976") {
+        window.open("english-laws/the-land-development-tax-ordinance-1976.html");
+    }
+
     if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
         window.open("404-not-found.html");
     }

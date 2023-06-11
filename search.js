a9c2eaae339d9e53c380a2fdc27967811328c9b1
sadmanalam142 <sadmanalam142@gmail.com>
2023-06-11 15:32:14 +0600

    3 600+ law have been added

--- search.js
+++ search.js
@@ -6,44 +6,8 @@ function openPage() {
         window.open("general-laws/bangladesh-songbidhan.html");
     }
 
-    if (a === "দেওয়ানী কার্যবিধি আইন, ১৯০৮") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "ফৌজদারি কার্যবিধি আইন, ১৮৯৮") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "দণ্ডবিধি আইন ,১৮৬০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "সাক্ষ্য আইন, ১৯৭২") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "তামাদি আইন, ১৯০৮") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "সুনির্দিষ্ট প্রতিকার আইন ১৮৭৭") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "নারী ও শিশু নির্যাতন দমন আইন, ২০০০") {
         window.open("general-laws/nari-o-sisu-nirzaton-domon-ayin-2000.html");
-    }
-
-    if (a === "বিশেষ ক্ষমতা আইন, ১৯৭৪") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "নেগোশিয়েবল ইনস্ট্রুমেন্ট অ্যাক্ট, ১৮৮১") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "অস্ত্র আইন, ১৮৭৮") {
-        window.open("404-not-found.html");
     }
 
     if (a === "মাদকদ্রব্য নিয়ন্ত্রণ আইন, ২০১৮") {
@@ -90,38 +54,6 @@ function openPage() {
         window.open("general-laws/jatio-porichoi-nibondhon-ayin-2010.html");
     }
 
-    if (a === "পুলিশ আইন, ১৮৬১") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "বিস্ফোরক দ্রব্য আইন, ১৯০৮") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "পারিবারিক আদালত অধ্যাদেশ, ১৯৮৫") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মুসলিম পারিবারিক আইন অধ্যাদেশ, ১৯৬১") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মুসলিম বিবাহ বিচ্ছেদ আইন, ১৯৩৯") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মুসলিম বিবাহ ও তালাক( রেজিস্ট্রিকরণ) আইন, ১৯৭৪") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "বাল্যবিবাহ বিরোধ আইন, ১৯২৯") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "প্রকাশ্য জুয়া আইন, ১৮৬৭") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "দ্রুত বিচার ট্রাইব্যুনাল আইন, ২০০২") {
         window.open("general-laws/druto-bichar-tribrunal-ayin-2002.html");
     }
@@ -130,28 +62,8 @@ function openPage() {
         window.open("general-laws/pornography-niyontron-ayin-2012.html");
     }
 
-    // if (a === "মানব পাচার প্রতিরোধ ও দমন আইন, ২০১২") {
-    //     window.open("404-not-found.html");
-    // }
-
-    if (a === "এসিড নিয়ন্ত্রণ (সংশোধন) আইন, ২০১০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মানিলন্ডারিং প্রতিরোধ আইন, ২০১২ [সংশোধন-২০১৫)") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "নির্যাতন এবং হেফাজতে মৃত্যু (নিবারণ) আইন, ২০১৩") {
         window.open("general-laws/nirzaton-abong-hefazote-mrittu-nibaron-ayin-2013.html");
-    }
-
-    if (a === "পাসপোর্ট আইন, ১৯২০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "বাংলাদেশ পাসপোর্ট আদেশ, ১৯৭৩") {
-        window.open("404-not-found.html");
     }
     
     if (a === "বাংলাদেশ স্ট্যান্ডার্ডস এন্ড টেস্টিং ইনস্টিটিউশন আইন, ২০১৮") {
@@ -162,100 +74,28 @@ function openPage() {
         window.open("general-laws/ojon-o-porimap-mrittudondo-ayin-2018.html");
     }
 
-    if (a === "অর্থ ঋণ আদালত আইন, ২০০৩ (সংশোধন-২০১০)") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "বৈদেশিক কর্মসংস্থান ও অভিবাসী আইন, ২০১৩") {
         window.open("general-laws/boideshik-kormosongsthan-o-ovibashi-ayin-2013.html");
-    }
-
-    if (a === "পাবলিক পরীক্ষা (অপরাধ) আইন, ১৯৮০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "পাসপোর্ট বিধিমালা, ১৯৫৫") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "ডিজিটাল নিরাপত্তা বিধিমালা, ২০২০") {
-        window.open("404-not-found.html");
     }
 
     if (a === "বাংলাদেশ সড়ক পরিবহণ কর্পোরেশন আইন, ২০২০") {
         window.open("general-laws/bangladesh-sorok-poribohon-corporation-ayin-2020.html");
     }
 
-    if (a === "মাদকদ্রব্য লাইসেন্স ও পারমিট ফিস (সংশোধনী), ২০২০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "নৌ-পুলিশ বিধিমালা, ২০২০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "বৈদেশিক কর্মসংস্থান ও অভিবাসী (রিক্রুটিং এজেন্ট শ্রেণিবিভাগ) বিধিমালা, ২০২০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "এন্টি টেররিজম ইউনিট বিধিমালা, ২০১৯") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "সরকারি কর্মচারী (নিয়মিত উপস্থিতি) বিধিমালা, ২০১৯") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "ট্যুরিস্ট পুলিশ বিধিমালা, ২০২০") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "আদালত কর্তৃক তথ্য-প্রযুক্তি ব্যবহার আইন, ২০২০") {
         window.open("general-laws/adalot-kotrik-projukti-babohar-ayin-2020.html");
     }
 
-    if (a === "কোম্পানী (সংশোধন) আইন, ২০২০") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "রেজিস্ট্রেশন ফিসের তালিকা (নতুন, ২০২০)") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "আইন-শৃঙ্খলা বিঘ্নকারী অপরাধ (দ্রুত বিচার) আইন, ২০০২") {
         window.open("general-laws/ayin-sringkhola-bighnokari-aporadh-druto-bichar-ayin-2002.html");
-    }
-
-    if (a === "বিস্ফোরক উপাদানাবলি আইন, ১৯০৮ (সংশোধন- ২০০২)") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "বিশেষ ক্ষমতা আইন, ১৯৭৪") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মাদক দ্রব্য নিয়ন্ত্রণ (লাইসেন্স ও পারমিট ফিস) বিধিমালা, ২০১৪") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "গৃহকর্মী সুরক্ষা ও কল্যাণ নীতি, ২০১৫") {
-        window.open("404-not-found.html");
     }
     
     if (a === "সড়ক পরিবহণ আইন, ২০১৮") {
         window.open("general-laws/sorok-poribohon-ayin-2018.html");
     }
 
-    if (a === "সন্ত্রাস বিরোধী আইন, ২০০৯ (সংশোধন-২০১৩)") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "বাল্যবিবাহ নিরোধ আইন, ২০১৭") {
         window.open("general-laws/ballobibaho-nirodh-ayin-2017.html");
-    }
-
-    if (a === "বাল্যবিবাহ নিরোধ বিধিমালা, ২০১৮") {
-        window.open("404-not-found.html");
     }
 
     if (a === "এসিড অপরাধ দমন আইন, ২০০২") {
@@ -264,10 +104,6 @@ function openPage() {
 
     if (a === "ডিঅক্সিরাইবোনিউক্লিক এসিড (ডিএনএ) আইন, ২০১৪") {
         window.open("general-laws/dioxiribonuclick-acid-dna-ayin-2014.html");
-    }
-
-    if (a === "ঢাকা মেট্রোপলিটন পুলিশ (অধঃস্তন কর্মকর্তাদের শৃঙ্খলা ও আপিল) বিধিমালা, ২০০৬") {
-        window.open("404-not-found.html");
     }
 
     if (a === "ব্যাংকার বহি সাক্ষ্য আইন, ২০২১") {
@@ -456,16 +292,8 @@ function openPage() {
 
 
     // latest laws
-    if (a === "আইন-শৃঙ্খলা বিঘ্নকারী অপরাধ (দ্রুত বিচার) (সংশোধন) আইন, ২০১৮") {
-        window.open("404-not-found.html");
-    }
-    
     if (a === "পারিবারিক সহিংসতা (প্রতিরোধ ও সুরক্ষা) আইন, ২০১০") {
         window.open("latest-laws/paribarik-sohingsota-protikar-o-surokha-ayin-2010.html");
-    }
-
-    if (a === "দুর্নীতি দমন কমিশন আইন, ২০০৪ (সংশোধন- ২০১৬)") {
-        window.open("404-not-found.html");
     }
 
     if (a === "অপরাধ সম্পর্কিত বিষয়ে পারস্পরিক সহায়তা আইন, ২০১২") {
@@ -478,18 +306,6 @@ function openPage() {
 
     if (a === "নারী ও শিশু নির্যাতন দমন (সংশোধন) আইন, ২০২০)") {
         window.open("latest-laws/nari-o-sisu-nirzaton-domon-songsodhon-ayin-2020.html");
-    }
-
-    if (a === "বিস্ফোরক উপাদানবলী (সংশোধন) আইন, ২০০২") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মোবাইল কোর্ট আইন. ২০০৯ (সংশোধন) আইন ২০১৭") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "তথ্য ও যোগাযোগ প্রযুক্তি আইন, ২০০৬ (সংশোধন) আইন-২০১৩") {
-        window.open("404-not-found.html");
     }
 
     if (a === "ভোক্তা অধিকার সংরক্ষণ আইন, ২০০৯") {
@@ -508,10 +324,6 @@ function openPage() {
         window.open("latest-laws/pita-matar-voron-pochon-ayin-2013.html");
     }
 
-    if (a === "ধুমপান ও তামাকজাত দ্রব্যের ব্যবহার ( নিয়ন্ত্রণ) আইন- ২০০৫( সংশোধন) ২০১৩") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "নিরাপদ খাদ্য আইন, ২০১৩") {
         window.open("latest-laws/nirapod-khaddo-ayin-2013.html");
     }
@@ -520,60 +332,12 @@ function openPage() {
         window.open("latest-laws/poribesh-adalot-ayin-2010.html");
     }
 
-    if (a === "বাংলাদেশ পরিবেশ সংরক্ষণ আইন, ১৯৯৫ ( সংশোধন) আইন ২০১০") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "মাতৃদুগ্ধ বিকল্প, শিশু খাদ্য, বাণিজ্যিকভাবে প্রস্ত্ততকৃত শিশুর বাড়তি খাদ্য ও উহা ব্যবহারের সরঞ্জামাদি (বিপণন নিয়ন্ত্রণ) আইন, ২০১৩") {
         window.open("latest-laws/matridugdho-bikolpo-sisu-khaddo-banijjikvabe-prostutkrito-sisur-barti-khaddo-o-uha-baboharer-sonjamadi-biponon-niyontron-ayin-2013.html");
     }
 
-    if (a === "রেলওয়ে আইন, ১৮৯০") {
-        window.open("404-not-found.html");
-    }
-
     if (a === "জাতীয় মানবাধিকার কমিশন আইন, ২০০৯") {
         window.open("latest-laws/jatio-manobadhikar-komishon-ayin-2009.html");
-    }
-
-    if (a === "মুসলিম বিবাহ ও তালাক (নিবদ্ধন) বিধিমালা, ২০০৯ (সংশোধনী-২০১১)") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "হস্তান্তরযোগ্য দলিল আইন, ১৮৮১") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "হস্তান্তরযোগ্য দলিল (সংশোধন আইন) ২০০৬") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "সন্ত্রাস বিরোধী আইন, ২০০৯ {সন্ত্রাস বিরোধী (সংশোধন) আইন, ২০১৩}") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "অ-কৃষি প্রজাস্বত্ব আইন, ১৯৪৯") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "বাড়ী ভাড়া নিয়ন্ত্রণ আইন, ১৯৯১") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "কোর্ট ফিস আইন, ১৮৭০(সংশোধন-২০১৬)") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "মামলা মূল্যায়ণ আইন- ১৮৮৭বাংলাদেশ পাসপোর্ট আদেশ, ৯৭১৩") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "ক্ষুদ্র বিচার বিষয়ক আদালত আইন, ১৮৮৭") {
-        window.open("404-not-found.html");
-    }
-
-    if (a === "অভিভাবক ও প্রতিপাল্য আইন, ১৮৯০") {
-        window.open("404-not-found.html");
     }
 
     if (a === "ব্যাংক আমানত বীমা আইন, ২০০০") {
@@ -604,6 +368,14 @@ function openPage() {
 
     if (a === "The Code of Civil Procedure, 1908") {
         window.open("english-laws/the-code-of-civil-procedure-1908.html");
+    }
+
+    if (a === "The Code of Criminal Procedure, 1898") {
+        window.open("english-laws/the-code-of-criminal-procedure-1898.html");
+    }
+
+    if (a === "The Penal Code, 1860") {
+        window.open("english-laws/the-penal-code-1860.html");
     }
     
     if (a === "The Evidence Act, 1872") {
